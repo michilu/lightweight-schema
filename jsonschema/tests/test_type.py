b921d8e20592014506bb5e0a2037ddcf7c3cb4d2
Ian Lewis <IanLewis@member.fsf.org>
2008-07-23 22:36:33 +0900

    Added test for string types

--- jsonschema/tests/test_type.py
+++ jsonschema/tests/test_type.py
@@ -16,3 +16,16 @@ class TestType(TestCase):
           pass
         else:
           self.fail("Expected failure for %s" % repr(x))
+    
+    def test_string(self):
+      for x in ["surrender?", "nuts!", "ok", "@hsuha", "\'ok?\'", "blah"]:
+        jsonschema.validate(x, {"type":"string"})
+      
+      #failures
+      for x in [1.2, 1, {"test":"blah"}, [32, 49], None, True]:
+        try:
+          jsonschema.validate(x, {"type":"string"})
+        except ValueError:
+          pass
+        else:
+          self.fail("Expected failure for %s" % repr(x))
