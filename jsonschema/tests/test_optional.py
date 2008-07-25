b7c0dccbec20b1b4e525922958ea6d5f99e128fa
Ian Lewis <IanLewis@member.fsf.org>
2008-07-25 21:27:51 +0900

    Added tests for the optional property

---
+++ jsonschema/tests/test_optional.py
@@ -0,0 +1,41 @@
+import math
+from unittest import TestCase
+
+import jsonschema
+
+class TestOptional(TestCase):
+  
+  props = {
+    "prop01": {"type":"string"},
+    "prop02": {"type":"number", "optional":False},
+    "prop03": {"type":"integer"},
+    "prop04": {"type":"boolean"}
+  }
+  schema = {"type": "object", "properties":props}
+  
+  
+  def test_optional1(self):
+    
+    x = {
+      "prop02":"blah"
+    }
+    
+    try:
+      jsonschema.validate(x, self.schema)
+    except ValueError, e:
+      self.fail("Unexpected failure: %s" % e)
+  
+  def test_optional2(self):
+    
+    x = {
+      "prop01":"test",
+      "prop03": 1,
+      "prop04": False
+    }
+    
+    try:
+      jsonschema.validate(x, self.schema)
+    except ValueError:
+      pass
+    else:
+      self.fail("Expected failure for %s" % repr(None))
